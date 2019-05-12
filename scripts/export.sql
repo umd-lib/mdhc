6fba17246f6b19887bbfefa7e2ad0bb2b54cb02f
Ben Wallberg <wallberg@umd.edu>
2019-05-12 11:18:56 -0400

    strip newlines and tabs from the export

--- scripts/export.sql
+++ scripts/export.sql
@@ -1,8 +1,8 @@
 SELECT
-  citation.author AS authors,
-  citation.citation,
-  IFNULL(citation.annotation,"") AS 'annotation/notes',
-  GROUP_CONCAT(codes.description SEPARATOR '; ') AS categories
+  TRIM(REPLACE(REPLACE(citation.author, "\n", ""), "\t", "")) AS authors,
+  TRIM(REPLACE(REPLACE(citation.citation, "\n", ""), "\t", "")) AS citation,
+  TRIM(REPLACE(REPLACE(IFNULL(citation.annotation,""), "\n", ""), "\t", "")) AS 'annotation/notes',
+  TRIM(REPLACE(REPLACE(GROUP_CONCAT(codes.description SEPARATOR '; '), "\n", ""), "\t", "")) AS categories
 FROM citation, codelookup, codes
 WHERE
   citation.ID=codelookup.CitID
